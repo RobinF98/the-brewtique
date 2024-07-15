4e5681db1c3d664e75d2f784242c9a17028fc269
RobinF98 <135007436+RobinF98@users.noreply.github.com>
2024-07-16 00:03:29 +0100

    Update allowed hosts setting

--- brewtique/settings.py
+++ brewtique/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "https://brewtique-12a59fe07437.herokuapp.com/",
+                ]
 
 
 # Application definition
