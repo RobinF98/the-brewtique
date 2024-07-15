797acb473d9df8aaf0f5dfca4412ef3f87889ab3
RobinF98 <135007436+RobinF98@users.noreply.github.com>
2024-07-16 00:00:00 +0100

    Update readme and settings

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # The Brewtique
 
+The Brewtique is a one-stop online coffee shop, stocking a wide variety of coffee and brewing equipment. The Brewtique caters to all sorts of coffee drinkers, from casual enjoyers to expert brewers.
+
+## Features
+
 ### Models
 
 * Product
-  * category FK
-  * sku
-  * name
-  * Price
-  * Strength
-  * Description
-  
+* Category
+
+### Thanks
+
+* [Coffee.ie](https://coffee.ie/) - for the products and dexcriptions used in the store
--- brewtique/settings.py
+++ brewtique/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tz&(=-jbkku98ftu&&+ph3=5f1f2dyaonpe%m4c**x_ppuz+#o'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = []
 
