0c2706b36ae6a3486f15e040882cbd501041903e
RobinF98 <135007436+RobinF98@users.noreply.github.com>
2025-03-15 18:06:05 +0000

    Feat: Product filtering and product details
    Add search functionality to all products page, and product details page

--- products/views.py
+++ products/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from webbrowser import get
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse
+from django.db.models import Q
 from .models import Category, Product
 
 # Create your views here.
@@ -8,9 +11,33 @@ def products(request):
     """ all products view """
 
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            # return all products page if no search query is entered
+            if not query:
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query
     }
 
     return render(request, "products/products.html", context)
+
+
+def product_details(request, product_id):
+    """ View to show product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, "products/product_detail.html", context)
