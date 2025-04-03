e0c4b55b9bcbbe5a5edf5d50015cfbee1554fac6
RobinF98 <135007436+RobinF98@users.noreply.github.com>
2025-04-03 02:08:55 +0100

    Upgrade allauth ver. and update allauth templates

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404, render
+from profiles.models import UserProfile
 
 def profile(request):
     '''Display user profile'''
 
+    profile = get_object_or_404(UserProfile, user=request.user)
+
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'profile': profile,
+    }
 
     return render(request, template, context)
