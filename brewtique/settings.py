6f57c9f52dc151180a66af007ea0433bb0d83ba1
RobinF98 <135007436+RobinF98@users.noreply.github.com>
2025-06-20 13:31:30 +0100

    Fix: Replace deprecated storage settings
    Replace DEFAULT_FILE_STORAGE with STORAGES dict to use cloudinary

--- brewtique/settings.py
+++ brewtique/settings.py
@@ -71,7 +71,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -193,9 +193,15 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 CLOUDINARY_STORAGE = {'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
                       'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
                       'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET')}
-
-
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+STORAGES = {
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    }
+}
+# DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
